054b8a509e71ad01f525767574b4c42293f9fcda
sanchayancse <45485694+sanchayancse@users.noreply.github.com>
2020-08-27 15:33:12 +0530

    no changes

--- hello_world/lib/main.dart
+++ hello_world/lib/main.dart
@@ -1,29 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:hello_world/screens/first_screen.dart';
 
-
-void main(){
+void main() {
   runApp(MyFlutterApp());
 }
 
-
-class MyFlutterApp extends StatelessWidget{
+class MyFlutterApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // ignore: todo
     // TODO: implement build
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         title: "My flutter app",
-
         home: Scaffold(
           appBar: AppBar(title: Text("My First Flutter App")),
           body: FirstScreen(),
-        )
-
-
-    );
-
-
+        ));
   }
-
 }
