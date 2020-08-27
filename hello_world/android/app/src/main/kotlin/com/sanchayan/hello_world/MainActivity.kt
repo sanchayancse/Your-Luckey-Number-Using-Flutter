d5e49628a724882f6083dc0f8f7285a4d60b83f0
sanchayancse <45485694+sanchayancse@users.noreply.github.com>
2020-08-27 15:17:28 +0530

    simple app
    first commit

---
+++ hello_world/android/app/src/main/kotlin/com/sanchayan/hello_world/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.sanchayan.hello_world
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ hello_world/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:hello_world/screens/first_screen.dart';
+
+
+void main(){
+  runApp(MyFlutterApp());
+}
+
+
+class MyFlutterApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: "My flutter app",
+
+        home: Scaffold(
+          appBar: AppBar(title: Text("My First Flutter App")),
+          body: FirstScreen(),
+        )
+
+
+    );
+
+
+  }
+
+}
